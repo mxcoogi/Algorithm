128104650393128b6dfd6c0169cbf48e76ef9aaa
mxcoogi <mxcoogi@gmail.com>
2025-04-04 07:21:41 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT i.animal_id, i.animal_type, i.name from animal_ins i 
+join animal_outs o on i.animal_id = o.animal_id
+where i.sex_upon_intake like 'intact%'
+and o.sex_upon_outcome not like 'intact%';
