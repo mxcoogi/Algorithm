bb074b26fecfc431673cdb8a6c441459d7dab244
mxcoogi <mxcoogi@gmail.com>
2025-04-09 00:46:18 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 22028 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -12,15 +12,25 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int len = Integer.parseInt(br.readLine());
         String str = br.readLine();
+        int answer = 0;
         int cnt = 0;
-        int size = 2 * n + 1;
-        String st = "I" + "OI".repeat(n);
-        for(int i = 0; i < len; i++){
-            if(str.substring(i).startsWith(st)){
+        char[] arr = str.toCharArray();
+        int i = 0;
+        while(i < len-2){
+            if(arr[i] == 'I' && arr[i+1] == 'O' && arr[i+2] == 'I'){
                 cnt++;
+                i+=2;
+            }
+            else {
+                cnt = 0;
+                i++;
+            }
+            if(cnt == n){
+                answer++;
+                cnt--;
             }
         }
-        System.out.println(cnt);
+        System.out.println(answer);
 
     }
 
