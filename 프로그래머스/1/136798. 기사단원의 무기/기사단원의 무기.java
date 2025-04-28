d506094d2c7f78e6bcda6654078613143c3e9f98
mxcoogi <mxcoogi@gmail.com>
2025-04-28 09:59:21 +0900

    [level 1] Title: 기사단원의 무기, Time: 90.51 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,24 +1,27 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-
+        int[] primeCnt = new int[number+1];
         int total = 0;
         for(int i = 1; i <= number; i++){
             int cnt = primeCnt(i);
-
+            primeCnt[i] = cnt;
             if(cnt > limit){
                 total += power;
             }else{
                 total += cnt;
             }
         }
+        
         return total;
     }
-
     static int primeCnt(int n){
-        int cnt = 1;
-        for(int i = 1; i <= n / 2; i++){
-            if(n % i == 0){
-                cnt++;
+        int cnt = 0;
+        for(int i = 1; i * i <= n; i++){
+            if(i*i == n) cnt++;
+            else{
+                if(n % i == 0){
+                    cnt+=2;
+                }
             }
         }
         return cnt;
