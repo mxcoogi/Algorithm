462cca0ff422a59edc90ba8331df9ba298fa8503
mxcoogi <mxcoogi@gmail.com>
2025-05-20 10:26:44 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.23 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int n) {
+        if(n <= 2){
+            return n;
+        }
+        long[] dp = new long[n+1];
+        dp[1] = 1L; dp[2] = 2L;
+        for(int i = 3; i < n+1; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+        }
+        return dp[n] % 1234567;
+    }
+}
