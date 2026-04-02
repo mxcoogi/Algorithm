e582a88748df20d91eef0b09e537ec44ddff5fe7
mxcoogi <mxcoogi@gmail.com>
2026-04-02 22:38:46 +0900

    [Bronze IV] Title: 주사위 세개, Time: 104 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 주사위 눈 3개 입력 받기
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+
+        if (a == b && b == c) {
+            result = 10000 + a * 1000;
+        }
+        else if (a == b || a == c) {
+            result = 1000 + a * 100;
+        }
+        else if (b == c) {
+            result = 1000 + b * 100;
+        }
+        else {
+            int max = Math.max(a, Math.max(b, c));
+            result = max * 100;
+        }
+
+        System.out.println(result);
+    }
+}
