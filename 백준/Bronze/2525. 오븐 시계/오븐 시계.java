74cacf75182d5d6f7c54cd72947e31f2d4b13866
mxcoogi <mxcoogi@gmail.com>
2026-04-02 22:25:06 +0900

    [Bronze III] Title: 오븐 시계, Time: 120 ms, Memory: 16060 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력 받기 위한 BufferedReader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken())+ Integer.parseInt(br.readLine());
+        hour = (hour % 24 + minute / 60) % 24;
+        minute = minute % 60;
+
+        System.out.println(hour +" "+ minute);
+
+
+    }
+
+}
