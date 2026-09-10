c58350904b384e94e23355d1ad25d7b8f22331cf
mxcoogi <mxcoogi@gmail.com>
2026-09-10 23:03:08 +0900

    Time: 39 ms (6.24%), Space: 47.7 MB (8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.kt
+++ 0125-valid-palindrome/0125-valid-palindrome.kt
@@ -3,13 +3,9 @@ class Solution {
     val lowercase = s.replace(Regex("[^A-Za-z0-9]"), "").lowercase()
     var left: Int = 0
     var right: Int = lowercase.length - 1
-    while (left <= right) {
-        if(lowercase[left] != lowercase[right]){
-            return false
-        }
-        left++
-        right--
+    if(lowercase.reversed() == lowercase){
+        return true
     }
-    return true
+    return false
 }
 }
