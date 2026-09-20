966ef1175b93aec089426ebf321c47d4b8b795b9
mxcoogi <mxcoogi@gmail.com>
2026-09-20 15:19:39 +0900

    Time: 3 ms (63.78%), Space: 45.3 MB (98.25%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,11 +1,12 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        val map = mutableMapOf<Int, Int>();
-        nums.forEachIndexed { index, i -> map[i] = index }
+        val map = mutableMapOf<Int, Int>()
         for ((index, value) in nums.withIndex()) {
             val need = target - value
             if(map.containsKey(need) && map[need] != index){
                 return intArrayOf(map[need]!!, index)
+            }else{
+                map[value] = index
             }
         }
         return intArrayOf()
